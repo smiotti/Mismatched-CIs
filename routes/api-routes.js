0a8edd8817902279a3926fc9820f9ebfd66a64c8
Mirian <mirianejerenwa@yahoo.com>
2019-03-10 15:47:36 -0400

    updated codes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,31 +1,7 @@
-const db = require('../models');
-
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
-module.exports = function(app) {
-  
-
-  // POST Request
-  // Adds a new product to our database
-  // Responds with success: true or false if successful
-  app.post('/api/login', function(req, res) {
-    // Check client side data that was sent
-    console.log (req.body);
-    // Find if user exist in database
-    // db.Users.create(req.body).then(function(rows) {
-    //   res.json({ success: true });
-    // }).catch(function(error) {
-    //   res.json({ error: error })
-    // });
-  });
-
-};
 //require models library
 const RestfulAPI = require('./RestClass');
 const models = require('../models');
+
 //restAPI
 module.exports = function (app) {
 
@@ -36,3 +12,25 @@ module.exports = function (app) {
   mismatched_ci.delete('id');
   mismatched_ci.update('id');
 }
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+// module.exports = function(app) {
+  
+//   // POST Request
+//   // Adds a new product to our database
+//   // Responds with success: true or false if successful
+//   app.post('/api/login', function(req, res) {
+//     // Check client side data that was sent
+//     console.log (req.body);
+//     // Find if user exist in database
+//     // db.Users.create(req.body).then(function(rows) {
+//     //   res.json({ success: true });
+//     // }).catch(function(error) {
+//     //   res.json({ error: error })
+//     // });
+//   });
+
+// };
+
---
+++ tempData/credential.js
@@ -0,0 +1,25 @@
+module.exports=[
+    {
+      name:"steve.miotti@company.com",
+      password:"steve",
+      avatar: "https://api.adorable.io/avatars/144/ronnie"
+      },
+    {
+      name:"nicastro.maryrose@company.com",
+      password:"nicastro",
+      avatar: "https://api.adorable.io/avatars/144/1p2io3j012j3e2112"
+
+    },
+    {
+        name:"Ricky",
+        password:"3",
+        avatar: "https://api.adorable.io/avatars/144/ricky"
+
+    },
+    {
+        name:"Mike",
+        password:"4",
+        avatar: "https://api.adorable.io/avatars/144/1231m1io"
+
+      }
+    ];
